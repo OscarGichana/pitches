abd2d9f688025a313d2b26f401f0a1465a2570cc
Oscar Gichana <gichanaoscar3@gmail.com>
2021-03-03 00:13:54 +0300

    deployment to heroku

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = 'Flask WTF Secret Key'
-MAIL_USERNAME = 'ossygicchy@gmail.com'
+MAIL_USERNAME = 'gichanaoscar3@gmail.com'
 MAIL_PASSWORD = 37600001128
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:123@localhost/pitching'
+DATABASE_URL = 'postgresql+psycopg2://oscar:123@localhost/pitching'
