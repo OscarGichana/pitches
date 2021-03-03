4041b3f63ed22b26a35266f051d9e72aadaf1c89
Oscar Gichana <gichanaoscar3@gmail.com>
2021-03-03 15:34:04 +0300

    add secure_file name

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export SECRET_KEY='Flask WTF Secret Key'
 export MAIL_USERNAME='gichanaoscar3@gmail.com'
 export MAIL_PASSWORD=37600001128
-export SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://oscar:123@localhost/pitching'
 
 python3.6 manage.py server
